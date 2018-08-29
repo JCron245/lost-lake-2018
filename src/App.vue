8514d6e76689db96f57f8887cf14ec27c40c823b
Jon Cron <jonnmb@gmail.com>
2018-08-29 10:30:41 -0600

    Lets add some page transitions!

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,14 @@
       </b-navbar>
     </header>
 		<main class="container">
-			<router-view/>
+			<transition 
+				name="fade" 
+				mode="out-in" 
+				@beforeLeave="beforeLeave" 
+				@enter="enter" 
+				@afterEnter="afterEnter">
+				<router-view/>
+			</transition>
 		</main>
     <footer class="container">
       <b-navbar toggleable="md">
@@ -52,7 +59,29 @@ export default {
     "b-navbar-toggle": bNavbarToggle,
     "b-collapse": bCollapse,
     "b-nav-item": bNavItem
-  }
+	},
+	data() {
+    return {
+      prevHeight: 0,
+    };
+  },
+  methods: {
+    beforeLeave(element) {
+      this.prevHeight = getComputedStyle(element).height;
+    },
+    enter(element) {
+      const { height } = getComputedStyle(element);
+
+      element.style.height = this.prevHeight;
+
+      setTimeout(() => {
+        element.style.height = height;
+      });
+    },
+    afterEnter(element) {
+      element.style.height = 'auto';
+    },
+  },
 };
 </script>
 
@@ -134,5 +163,16 @@ nav.navbar {
 }
 #copyright {
   font-size: .75em;
+}
+.fade-enter-active,.fade-leave-active {
+  transition-duration: 0.29s;
+  transition-property: height, opacity;
+  transition-timing-function: ease;
+	overflow: hidden;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
 }
 </style>
